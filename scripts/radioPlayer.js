24c14bede88feda7f5a27e19847cfa6cd00f5a08
Victor Tsisar <tsisar.victor@gmail.com>
2020-12-23 17:05:26 +0200

    complement bar videoPlayer.js and init radioPlayer.js

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,60 @@
 export const radioPlayerInit = () => {
     console.log('Radio Init');
+
+    const radio = document.querySelector('.radio'),
+        radioCoverImg = document.querySelector('.radio-cover__img'),
+        radioHeaderBig = document.querySelector('.radio-header__big'),
+        radioNavigation = document.querySelector('.radio-navigation'),
+        radioItem = document.querySelectorAll('.radio-item'),
+        radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;  // Кнопка играть / пауза блокирована
+
+    // Смена вида кнопки играть / пауза
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');  // анимация вращения отключена
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');  // анимация вращения включена
+            radioStop.classList.remove('fa-play');
+            radioStop.classList.add('fa-stop');
+        }
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        
+        const parrent = target.closest('.radio-item');  // получение даных радиостанции
+        radioItem.forEach(item => item.classList.remove('select'));  // обводка активной станции
+        parrent.classList.add('select');
+        const title = parrent.querySelector('.radio-name').textContent;  // вывести название активной станции
+        radioHeaderBig.textContent = title;
+        const urlImg = parrent.querySelector('.radio-img').src;  // получение иконки радиостанции
+        radioCoverImg.src = urlImg;
+
+
+        radioStop.disabled = false;  // снятие блока с кнопки после выбора станции
+
+        console.log(target.dataset.radioStantion);
+        audio.src = target.dataset.radioStantion;  // получение адреса радиостанции
+
+        audio.play();
+        changeIconPlay();
+    });
+
+    // Кнопка играть / пауза
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+
+        changeIconPlay();
+    });
 }
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -6,7 +6,11 @@ export const videoPlayerInit = () => {
         videoButtonStop = document.querySelector('.video-button__stop'),
         videoTimePassed = document.querySelector('.video-time__passed'),
         videoProgress = document.querySelector('.video-progress'),
-        videoTimeTotal = document.querySelector('.video-time__total');
+        videoTimeTotal = document.querySelector('.video-time__total'),
+        videoIconMute = document.querySelector('.video-icon__mute'),
+        videoIconTalk = document.querySelector('.video-icon__talk'),
+        videoVolume = document.querySelector('.video-volume'),
+        videoFullscreen = document.querySelector('.video-fullscreen');
 
     // Работа с кнопкой Играть/Пауза
     const toggleIcon = () => {
@@ -18,8 +22,11 @@ export const videoPlayerInit = () => {
             videoButtonPlay.classList.remove('fa-play');
         }
     }
+
     // Запуск плеера и пауза
-    const togglePlay = () => {
+    const togglePlay = (event) => {
+        event.preventDefault();
+
         if (videoPlayer.paused) {
             videoPlayer.play();
         } else {
@@ -38,11 +45,45 @@ export const videoPlayerInit = () => {
     // Добавление нулей в счётчик времени
     const addZero = n => n < 10 ? '0' + n : n;
 
+    // Значок отключения звука
+    const changeRangeValue = () => {
+        if (videoPlayer.volume === 0) {
+            videoIconMute.classList.add('mute');
+        }
+        if (videoPlayer.volume !== 0) {
+            videoIconMute.classList.remove('mute');
+        }
+    }
+
+    // Шкала громкости
+    const changeValue = () => {
+        const valueVolume = videoVolume.value;
+        videoPlayer.volume = valueVolume / 100;
+
+        changeRangeValue();        
+    }
+
+    // Включить/выключить звук
+    const changeVolume = () => {
+        videoIconTalk.addEventListener('click', () => {
+            videoIconMute.classList.remove('mute');
+            videoPlayer.volume = 1;
+            videoVolume.value = videoPlayer.volume * 100;
+        });
+
+        videoIconMute.addEventListener('click', () => {
+            videoIconMute.classList.toggle('mute');
+            videoPlayer.volume = 0;
+            videoVolume.value = videoPlayer.volume * 0;
+        });
+    }
+
     videoPlayer.addEventListener('click', togglePlay);
     videoButtonPlay.addEventListener('click', togglePlay);
     videoPlayer.addEventListener('play', toggleIcon);
     videoPlayer.addEventListener('pause', toggleIcon);
     videoButtonStop.addEventListener('click', stopPlay);
+    videoVolume.addEventListener('input', changeValue);
 
     // Счётчик времени плеера и ползунка
     videoPlayer.addEventListener('timeupdate', () => {
@@ -61,14 +102,30 @@ export const videoPlayerInit = () => {
 
         videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
         videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+    });
 
-        // Связь ползунка и видео 
-        videoProgress.addEventListener('change', () => {
-            const duration = videoPlayer.duration;
-            const value = videoProgress.value;
+    // Связь ползунка и видео 
+    videoProgress.addEventListener('input', () => {
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
 
-            videoPlayer.currentTime = (value * duration) / 100;
-        });
+        videoPlayer.currentTime = (value * duration) / 100;
+    });
 
-    })
+    // Открыть на полный екран
+    videoFullscreen.addEventListener('click', () => {
+        videoPlayer.requestFullscreen();
+    });
+
+    // Связь шкалы громкости полноекранной версии и дефолтной
+    videoVolume.addEventListener('input', changeValue);
+    videoPlayer.addEventListener('volumechange', () => {
+        videoVolume.value = Math.round(videoPlayer.volume * 100);
+
+        changeRangeValue();
+    });
+
+    // Вызов функции для установления значения громкости видео 
+    changeValue();  // по умолчанию
+    changeVolume();  // кнопки 0 - 100
 }
